b5a8f0a0b082d254405843d24eebc23d05496913
朴智康 <piaozhikang@58ganji.com>
2019-02-18 12:03:32 +0800

    [update]override auto-config using manual config

--- src/main/java/com/example/readinglist/entity/Reader.java
+++ src/main/java/com/example/readinglist/entity/Reader.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -51,31 +50,31 @@ public class Reader implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
